5a2e04a9a7a4257bc1b43fb6e540b550c52aa9c5
amajidsinar <amajidsinar@gmail.com>
2017-08-22 18:56:56 +0700

    more consistent training set and easier test set

--- data juli nov 2013 sinabung/Run/FCN_WaveClassification.py
+++ data juli nov 2013 sinabung/Run/FCN_WaveClassification.py
@@ -7,18 +7,20 @@ Created on Tue Aug  8 09:39:47 2017
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import EarlyStopping
 
 import random
 
 random.seed(5)
 
-training_set = pd.read_csv("dataset/training-set.csv", delimiter=";", header=None)
+training_set = pd.read_csv("dataset/training-set.csv", delimiter=",", header=None)
 X_train = training_set.iloc[:,1:].values
 y_train = training_set.iloc[:,0:1].values
 batch_size = min(X_train.shape[0]/10, 16)
 
 #Import test set
-test_set = pd.read_csv("dataset/test-set.csv", delimiter=";", header=None)
+test_set = pd.read_csv("dataset/test-set.csv", delimiter=",", header=None)
 X_test = test_set.iloc[:,1:].values
 y_test = test_set.iloc[:,0:1].values
 
@@ -36,20 +38,24 @@ X_test = np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 #Importing the CNN libraries
 from keras.models import Sequential
 from keras.layers import Conv1D,GlobalAveragePooling1D
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.layers.normalization import BatchNormalization
 
 #FCN
 classifier = Sequential()
 
 #Convolution and MaxPooling
-classifier.add(Conv1D(filters=128, kernel_size=8, activation='relu', strides=1, input_shape=(X_train.shape[1],1)))
+classifier.add(Conv1D(filters=128, kernel_size=8, activation='relu', strides=1, input_shape=(X_train.shape[1],1),
+                                                                                            border_mode='same'))
+classifier.add(Dropout(0.2))
 classifier.add(BatchNormalization())
 
-classifier.add(Conv1D(filters=256, kernel_size=5, strides=1, activation='relu'))
+classifier.add(Conv1D(filters=256, kernel_size=5, strides=1, activation='relu', border_mode='same'))
+classifier.add(Dropout(0.2))
 classifier.add(BatchNormalization())
 
-classifier.add(Conv1D(filters=128, kernel_size=3, strides=1, activation='relu'))
+classifier.add(Conv1D(filters=128, kernel_size=3, strides=1, activation='relu', border_mode='same'))
+classifier.add(Dropout(0.2))
 classifier.add(BatchNormalization())
 
 classifier.add(GlobalAveragePooling1D())
@@ -63,7 +69,13 @@ print(classifier.summary())
 #Configure the learning process
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history=classifier.fit(X_train,y_train, batch_size=batch_size, epochs=1, validation_data=(X_test,y_test))
+earlyStopping=EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+
+reduce_lr = ReduceLROnPlateau(monitor = 'loss', factor=0.5, patience=50, min_lr=0.0001)
+
+history=classifier.fit(X_train,y_train, batch_size=batch_size, epochs=1000
+                       , validation_data=(X_test,y_test), callbacks=[reduce_lr,earlyStopping])
+
 
 print(history.history.keys())
 #  "Accuracy"
